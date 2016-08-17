618a157a7387288d1d2ede937e8a797767aaaa6b
2360 <2360@10.12.59.85>
2016-08-17 17:55:42 -0300

    Alteracao de TomadaFacade para que nao implemente AbstractFacade

--- src/java/br/jus/trt12/paulopinheiro/sati/redes/ejb/TomadaFacade.java
+++ src/java/br/jus/trt12/paulopinheiro/sati/redes/ejb/TomadaFacade.java
@@ -1,33 +1,20 @@
 package br.jus.trt12.paulopinheiro.sati.redes.ejb;
 
-import br.jus.trt12.paulopinheiro.sati.exceptions.SatiLogicalException;
-import br.jus.trt12.paulopinheiro.sati.geral.ejb.comum.AbstractFacade;
 import br.jus.trt12.paulopinheiro.sati.redes.model.Tomada;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Stateless
-public class TomadaFacade extends AbstractFacade<Tomada> {
+public class TomadaFacade {
     @PersistenceContext(unitName = "satiPU")
     private EntityManager em;
 
-    public TomadaFacade() {
-        super(Tomada.class);
+    public Tomada find(Object id) {
+        return getEntityManager().find(Tomada.class, id);
     }
 
-    @Override
     protected EntityManager getEntityManager() {
         return this.em;
-    }
-
-    @Override
-    public void salvar(Tomada tomada) throws SatiLogicalException {
-        throw new UnsupportedOperationException("Not supported"); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void excluir(Tomada tomada) throws SatiLogicalException {
-        throw new UnsupportedOperationException("Not supported"); //To change body of generated methods, choose Tools | Templates.
     }
 }
